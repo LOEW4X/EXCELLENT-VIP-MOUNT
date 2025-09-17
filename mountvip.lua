4207b2fb3d709f9b4114af8c85083b5eb9b86ae4
LOEW4X <95996724+LOEW4X@users.noreply.github.com>
2025-09-17 19:34:37 +0700

    mountvip.lua

--- mountvip.lua
+++ mountvip.lua
@@ -63,11 +63,11 @@ local Window = Rayfield:CreateWindow({
 })
 
 -- Tabs
-local TeleportTab = Window:CreateTab("Teleport", 🔁)
-local PlayerTab = Window:CreateTab("Players", 🚹)
+local TeleportTab = Window:CreateTab("Teleport", 4483362458)
+local PlayerTab = Window:CreateTab("Players", 4483362458)
 local UtilityTab = Window:CreateTab("Utility", 4483362458)
-local PerformanceTab = Window:CreateTab("Performance", 🎮)
-local WeatherTab = Window:CreateTab("Weather", ⛅)
+local PerformanceTab = Window:CreateTab("Performance", 4483362458)
+local WeatherTab = Window:CreateTab("Weather", 4483362458)
 
 -- Notification helper (bottom-right)
 local function notifyBottomRight(text, duration)
